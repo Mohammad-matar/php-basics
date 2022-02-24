70ace1116f0b4a02ca9bbc51a3a3b38b892edca1
Mohammad-Matar <moe.matar1998@gmail.com>
2022-02-24 12:37:34 +0200

    basic-1-step3

---
+++ step_3.php
@@ -0,0 +1,9 @@
+<?php
+$name ='Mohammad';
+$email ='moe.matar1998@gmail.com';
+echo " My name is $name and my email is $email";
+$height = 12;
+$width = 50;
+$area = $height * $width;
+echo " $area";
+?>
