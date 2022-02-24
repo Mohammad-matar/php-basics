69e4f5f053bd87e2eb86a0f6b7adf0639a1808df
Mohammad-Matar <moe.matar1998@gmail.com>
2022-02-24 16:59:53 +0200

    another-solution-for-step6

--- step_6.php
+++ step_6.php
@@ -1,9 +1,8 @@
 <?php 
-function calculateArea() {
-    $height= 5;
-    $width = 3;
+function calculateArea($height,$width) {
+    
     $area= $height * $width ;
     echo "$area"; // we should print insite the function
 }
-calculateArea() // call the function
+calculateArea(5,3) // call the function
 ?>
--- step_7.php
+++ step_7.php
@@ -5,5 +5,7 @@ $myage =  $current_year - $birth_year ;
 if ($myage > 18) {echo "You can drive";} else{
     echo " You still a kid, go and play GTA";
 }
+//zedna variable myage la na3reff lal na2ess l current year mn l birth year 
+// haydi ; lezem ten7at  abel teskiret l semi colon ba3d l string
 
 ?>
