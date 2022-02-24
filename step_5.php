58461b93dc89acfcf9476e42ef46f5845e93b343
Mohammad-Matar <moe.matar1998@gmail.com>
2022-02-24 16:17:53 +0200

    basic1-step5

---
+++ step_5.php
@@ -0,0 +1,10 @@
+<?php 
+$course_name = 'Querry';
+$enrolled_students= 24;
+$price= 12.33;
+$on_discount=false;
+echo "1 Course title: ".$course_name."\n".
+"2 Enrolled Students: " .$enrolled_students."\n".
+"3 Course price: " .$price." $ USD \n".
+"4 Course on discount: " .($on_discount?"Yes \n":"No \n");
+?>
