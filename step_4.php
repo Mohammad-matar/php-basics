11481f61a511e7af0a01fa72f6e514804dbd52bf
Mohammad-Matar <moe.matar1998@gmail.com>
2022-02-24 14:57:09 +0200

    basic1-step4

---
+++ step_4.php
@@ -0,0 +1,8 @@
+<?php
+$name = 'Salim';
+$position = 'Developper';
+$github = 'https://github.com/Mohammad-matar';
+echo "Hello,".$name.",Im a ".$position." please check my github link ".$github;
+echo  "Hello, $name ,Im a $position please check my github link $github";
+echo 'Hello '.$name. ',Im a '.$position.' please check my github link '.$github;
+?>
