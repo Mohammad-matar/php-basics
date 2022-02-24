35c308dbcabba4a7ea497ea5064fec3ab9e39a08
Mohammad-Matar <moe.matar1998@gmail.com>
2022-02-24 18:55:35 +0200

    basic1-step-8b

---
+++ step_8b.php
@@ -0,0 +1,9 @@
+<?php
+$eggs = array ('balade','mazere3');
+$milk= array('Fresh', 'Taanayel');
+$water_pack= array ( 'Tanoureen', 'Reem');
+
+echo "Hey Sir, Please I need 1 pack of " 
+.$eggs[0]." and 3 "
+.$water_pack[1]. " Water Pack";
+?>
