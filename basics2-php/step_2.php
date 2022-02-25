920b5c645de6e4ab8bb48c8b42cb51cfd3db88dc
Mohammad-Matar <moe.matar1998@gmail.com>
2022-02-25 16:52:39 +0200

    basics2-step_2

--- basics2-php/step_2.php
+++ basics2-php/step_2.php
@@ -1,3 +1,16 @@
 <?php
+function greaterThan($num){
+    if ($num > 30) {echo "greater than than 30";}
 
+    else if ($num > 20)
+
+    {echo "greater than than 20";} 
+
+    else if ($num > 10)
+
+    {echo "greater than than 10";}
+
+    else {echo "less than 10";}
+}
+    greaterThan(31);
 ?>
