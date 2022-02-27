30365b352c36dc9e96871c17de05b9c76de22609
Mohammad-Matar <moe.matar1998@gmail.com>
2022-02-27 17:27:31 +0200

    basics2_step5-6

---
+++ basics2-php/step_6.php
@@ -0,0 +1,12 @@
+<?php
+    // $num = 245354;
+    // $numlength = strlen((string)$num);
+    // echo strlen((string)$num[1]);
+
+    // $str = "matarov";
+    // $len = strlen(($str));
+    // echo $len;
+
+    $str = "matarOv";
+    echo strlen ((string)$str);
+?>
